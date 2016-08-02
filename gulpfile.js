642fa1cb95e0df89973f5404e91cb733c4d13ebb
Marty Fitchen <mfitchen@gmail.com>
2016-08-02 08:05:06 -0700

    edit a few lines of code in the Gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
 var gulp = require('gulp');
-var utilities = require('gulp-util');
 // JavaScript build tasks go in the order concatInterface -> jsBrowserify -> minifyScripts
 var concat = require('gulp-concat');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var uglify = require('gulp-uglify');
+var utilities = require('gulp-util');
 var del = require('del');
 var buildProduction = utilities.env.production;
 var jshint = require('gulp-jshint');
@@ -45,13 +45,13 @@ gulp.task("minifyScripts", ["jsBrowserify"], function(){
     .pipe(gulp.dest("./build/js"));
 });
 
-gulp.task("build", function(){
-  if (buildProduction) {
-    gulp.start('minifyScripts');
-  } else {
-    gulp.start('jsBrowserify');
-  }
-});
+// gulp.task("build", function(){
+//   if (buildProduction) {
+//     gulp.start('minifyScripts');
+//   } else {
+//     gulp.start('jsBrowserify');
+//   }
+// });
 
 gulp.task("clean", function(){
   return del(['build', 'tmp']);
