7b6c326888571291fff556012625fd2c0c44c2a0
Marty Fitchen <mfitchen@gmail.com>
2016-08-01 21:25:51 -0700

    add working files now using Bower and API calls

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,26 @@
 var gulp = require('gulp');
-
 var utilities = require('gulp-util');
-
 // JavaScript build tasks go in the order concatInterface -> jsBrowserify -> minifyScripts
-
 var concat = require('gulp-concat');
-
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
-
 var uglify = require('gulp-uglify');
-
 var del = require('del');
-
 var buildProduction = utilities.env.production;
+var jshint = require('gulp-jshint');
+// var lib = require('bower-files')();
+var lib = require('bower-files')({
+  "overrides":{
+    "bootstrap" : {
+      "main": [
+        "less/bootstrap.less",
+        "dist/css/bootstrap.css",
+        "dist/js/bootstrap.js"
+      ]
+    }
+  }
+});
+var browserSync = require('browser-sync').create();
 
 
 // JS BUILD TASK | concatInterface
@@ -60,10 +67,58 @@ gulp.task("build", ['clean'], function(){
 
 // put this task at the very bottom of our gulpfile since we will run it separately from the chain of build tasks... this is a good task to run periodically as you develop, and when you need some help debugging
 
-var jshint = require('gulp-jshint');
-
 gulp.task('jshint', function(){
   return gulp.src(['js/*.js'])
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
+});
+
+gulp.task('bowerJS', function () {
+  return gulp.src(lib.ext('js').files)
+    .pipe(concat('vendor.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('bowerCSS', function () {
+  return gulp.src(lib.ext('css').files)
+    .pipe(concat('vendor.css'))
+    .pipe(gulp.dest('./build/css'));
+});
+
+gulp.task('bower', ['bowerJS', 'bowerCSS']);
+
+gulp.task('build', ['clean'], function(){
+  if (buildProduction) {
+    gulp.start('minifyScripts');
+  } else {
+    gulp.start('jsBrowserify');
+  }
+  gulp.start('bower');
+});
+
+gulp.task('serve', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./",
+      index: "index.html"
+    }
+  });
+
+  gulp.watch(['js/*.js'], ['jsBuild']);
+  gulp.watch(['bower.json'], ['bowerBuild']);
+  gulp.watch(['*.html'], ['htmlBuild']);
+
+});
+
+gulp.task('jsBuild', ['jsBrowserify', 'jshint'], function(){
+  browserSync.reload();
+});
+
+gulp.task('bowerBuild', ['bower'], function(){
+  browserSync.reload();
+});
+
+gulp.task('htmlBuild', function() {
+  browserSync.reload();
 });
